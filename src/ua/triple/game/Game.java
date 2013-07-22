1ed5fc62f855595c8afd64ae9fab365982413c5d
eliotik <eliotik@gmail.com>
2013-07-22 21:16:07 +0300

    added size to game window

--- src/ua/triple/game/Game.java
+++ src/ua/triple/game/Game.java
@@ -3,5 +3,16 @@ package ua.triple.game;
 import java.awt.*;
 
 public class Game extends Canvas {
+    public static Dimension size = new Dimension(800, 600);
+
+    public Game(int height, int width) {
+        Dimension d = new Dimension(height, width);
+        setSize(d);
+        setPreferredSize(d);
+    }
+    public Game() {
+        setSize(size);
+        setPreferredSize(size);
+    }
 
 }
